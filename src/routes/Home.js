45b6b24553d24838734380fd70c1b4067d4eab87
kawaharm <mjbk295@gmail.com>
2022-04-19 15:32:55 -0700

    test

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import Header from "../components/Header";
-import "./Home.css";
+import Header from '../components/Header';
+import './Home.css';
 
 function Home() {
   return (
     <>
       <div className="home-container">
-        <h1>WELCOME TO CAMPUS BOPS!</h1>
+        <h1>WELCOME TO CAMjjjjjPUS BOPS!</h1>
         <p>Discover the best music heard around campus voted by you!</p>
       </div>
     </>
