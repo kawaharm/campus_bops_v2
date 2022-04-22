28b0625e2a5cafb0904b25b89e775c53f1fb8d08
kawaharm <mjbk295@gmail.com>
2022-04-21 21:44:07 -0700

    test

--- src/components/SchoolList.js
+++ src/components/SchoolList.js
@@ -1,60 +1,67 @@
-import React, { useEffect, useContext } from 'react';
-import { SchoolsContext } from '../context/CampusContext';
+import React, { useEffect, useContext } from "react";
+import { SchoolsContext } from "../context/CampusContext";
 import SchoolFinder from "../api/SchoolFinder";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SchoolList() {
-    const { schools, setSchools } = useContext(SchoolsContext);
-    let navigate = useNavigate();
+  const { schools, setSchools } = useContext(SchoolsContext);
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // Get all schools from server
-                const response = await SchoolFinder.get("/")
-                // Store school list in state
-                setSchools(response.data.data.schools);
-            } catch (err) {
-                console.log(err)
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // // Get all schools from server
+        // const response = await axios.get("/")
+        // // Store school list in state
+        // setSchools(response.data.data.schools);
 
-        fetchData();
-    }, [])
-
-    const handleSchoolSelect = (id) => {
-        try {
-            // Navigate to detail page
-            navigate(`/schools/${id}`);
-        } catch (err) {
-            console.log(err);
-        }
+        const response = await axios.get("/");
+        console.log("RESPONSE TEST: ", response);
+      } catch (err) {
+        console.log(err);
+      }
     };
 
-    return (
-        <div className="list-group">
-            <table className="table table-hover table-dark">
-                <thead>
-                    <tr className="bg-primary">
-                        <th scope="col">School</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* && Means will only render if schools exists */}
-                    {schools && schools.map(school => {
-                        return (
-                            <tr
-                                onClick={() => { handleSchoolSelect(school.id) }}
-                                key={school.id}
-                            >
-                                <td style={{ cursor: "pointer" }}>{school.name}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+    fetchData();
+  }, []);
+
+  const handleSchoolSelect = (id) => {
+    try {
+      // Navigate to detail page
+      navigate(`/schools/${id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">School</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* && Means will only render if schools exists */}
+          {schools &&
+            schools.map((school) => {
+              return (
+                <tr
+                  onClick={() => {
+                    handleSchoolSelect(school.id);
+                  }}
+                  key={school.id}
+                >
+                  <td style={{ cursor: "pointer" }}>{school.name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default SchoolList;
